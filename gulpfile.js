ac67df703ff0d514d255514916d362c5fd6eedcc
Bob Ke <961904564@qq.com>
2016-11-21 18:57:51 +0800

    Update and rename 终极版gulpfile.js to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
-
-// 下载插件代码
-//npm install --save-dev gulp-sass browser-sync gulp-imagemin gulp-gzip gulp-minify-css gulp-rename gulp-concat gulp-uglify gulp-notify gulp-cache
-
 // gulp配置文件
 
 var gulp = require('gulp'),  
