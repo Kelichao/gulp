88f31bdea3c96b1da6fa26134a1dc6dfdfe65e11
Bob Ke <961904564@qq.com>
2016-12-29 15:12:07 +0800

    去除了autoprefixer

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -38,10 +38,6 @@ gulp.task('styles', function() {
     gulp.src(['src/styles/main1.css','src/styles/main2.css','src/styles/main3.css'])
     .pipe(concat("all.css"))
     // .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer({browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
-    					cascade:true,
-    					remove:false
-}))
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
